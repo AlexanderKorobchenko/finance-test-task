ff89276a46f789e3f325a2477f39bdb026c44fc7
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2022-01-29 15:21:47 +0200

    created markup and styles on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Container from './components/Container';
 import Title from './components/Title';
+import List from './components/List';
 
 function App() {
   return (
     <Container>
       <Title>Financial statistics</Title>
+      <List />
     </Container>
   );
 }
---
+++ client/src/components/Item/Item.js
@@ -0,0 +1,26 @@
+import { FiArrowUp, FiArrowDown } from 'react-icons/fi';
+import s from './Item.module.css';
+
+function Item({ ticker }) {
+  const changes = ticker.change - ticker.price;
+  const percent = (Math.abs(changes) / ticker.price) * 100;
+
+  return (
+    <li className={s.item}>
+      <span className={s.name}>{ticker.ticker}</span>
+      <div className={s.box_price}>
+        <span className={s.price}>{ticker.change}$</span>
+        <span className={s.change} style={{ color: changes > 0 ? '#157533' : '#A11216' }}>
+          {changes > 0 && '+'}
+          {changes.toFixed(2)}$
+        </span>
+        <span className={s.change_percent} style={{ color: changes > 0 ? '#157533' : '#A11216' }}>
+          {changes > 0 ? <FiArrowUp /> : <FiArrowDown />}
+          {percent.toFixed(2)}%
+        </span>
+      </div>
+    </li>
+  );
+}
+
+export default Item;
